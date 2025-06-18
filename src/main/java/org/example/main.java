7aad90ba99e30c91975cce8465397ad2159bfc7b
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-18 12:20:22 +0530

    Create main.java

---
+++ src/main/java/org/example/main.java
@@ -0,0 +1,8 @@
+// src/main/java/org/example/Main.java
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Main");
+    }
+}
