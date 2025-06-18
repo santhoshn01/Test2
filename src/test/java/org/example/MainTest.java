f3145fba2ee30b14f07caf044fdeecebb4e35f9d
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-18 12:41:53 +0530

    Update MainTest.java

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainTest {
     @Test
-    void sampleTest(a,b) {
-        assertTrue(a = true);
+    void sampleTest() {
+        assertTrue(true);
     }
 }
