9b1b74a1caa29d03fc8dac33c2e001aa3807e383
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-20 13:15:12 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,16 @@
-// src/main/java/org/example/Main.java
 package org.example;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Main");
-        
+
+        // Unused variable - will trigger a code smell
+        int unusedVariable = 42;
+
+        try {
+            // Empty catch block - bad practice and usually flagged
+        } catch (Exception e) {
+        }
     }
 }
+
