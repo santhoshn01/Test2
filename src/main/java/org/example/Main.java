43434c4c493d129b67162262e179daac3eeb85e6
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-25 13:55:53 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from Main");
 
+        Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter username");
         // Unused variable - will trigger a code smell
         int unusedVariable = 42;
 
