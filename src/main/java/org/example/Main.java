6dd0c9663b4263a51ea7cbf7b3bc1a0b07fd0123
santhoshn01 <santhosh_n@chelsoft.com>
2025-06-25 13:56:53 +0530

    Scanner added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,6 +8,10 @@ public class Main {
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter username");
+
+        String userName = myObj.nextLine();  // Read user input
+        System.out.println("Username is: " + userName);
+        
         // Unused variable - will trigger a code smell
         int unusedVariable = 42;
 
